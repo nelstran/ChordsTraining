fa50b35805069778cca341d835743f3302703be7
nelstran <nelsonquoc2003@yahoo.com>
2022-05-11 14:47:27 -0600

    First Git commit for project

---
+++ ChordsTraining/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ChordsTraining
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chordLabel = new System.Windows.Forms.Label();
+            this.revealButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // chordLabel
+            // 
+            this.chordLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chordLabel.Location = new System.Drawing.Point(148, 5);
+            this.chordLabel.Name = "chordLabel";
+            this.chordLabel.Size = new System.Drawing.Size(203, 40);
+            this.chordLabel.TabIndex = 1;
+            this.chordLabel.Text = "label1";
+            this.chordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // revealButton
+            // 
+            this.revealButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revealButton.Location = new System.Drawing.Point(120, 243);
+            this.revealButton.Name = "revealButton";
+            this.revealButton.Size = new System.Drawing.Size(125, 40);
+            this.revealButton.TabIndex = 2;
+            this.revealButton.Text = "Reveal";
+            this.revealButton.UseVisualStyleBackColor = true;
+            this.revealButton.UseWaitCursor = true;
+            this.revealButton.Click += new System.EventHandler(this.revealButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(250, 243);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(125, 40);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 291);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.revealButton);
+            this.Controls.Add(this.chordLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Chords";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label chordLabel;
+        private System.Windows.Forms.Button revealButton;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ ChordsTraining/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChordsTraining
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Chord currChord = new Chord();
+        bool reveal = false;
+        /// <summary>
+        /// Class <c>Chord</c> Representation of a Piano Chord
+        /// </summary>
+        public struct Chord
+        {
+            public int root;
+            public int quality;
+            public int extend;
+            private static Point[] points;
+            private static Point cRoot = new Point(17, 170);
+            private static int radius = 7;
+            private static int[,] qualities = new int[5, 4] {
+                {0, 3, 4, 3 },//Minor
+                {0, 4, 3, 4 },//Major
+                {0, 3, 3, 3 },//Diminished
+                {0, 4, 4, 2 },//Augmented
+                {0, 4, 3, 3 } //7th
+            };
+
+            /// <summary>
+            /// Initialize a chord.
+            /// Root 0-11 (0 = C, 1 = C#, etc.)
+            /// Quality 0-5 (0 = Minor, 1 = Major, 2 = Dim, 3 = Aug, 4 = 7th)
+            /// Extend 0-1 0 = Triad, 1 = 7th
+            /// </summary>
+            /// <param name="root"></param>
+            /// <param name="quality"></param>
+            /// <param name="extend"></param>
+            public Chord(int root, int quality, int extend)
+            {
+                if (root > 11 || extend < 1 || quality < 5)
+                    throw new Exception("Invalid chord structure");
+                if(extend == 1 && quality == 4)
+                    throw new Exception("Triad cannot be a 7th Chord");
+
+                this.root = root;
+                this.quality = quality;
+                this.extend = extend;
+            }
+
+            /// <summary>
+            /// Generate a random chord
+            /// </summary>
+            public void generateChord()
+            {
+                Random rng = new Random();
+
+                //C to B (Easier to code)
+                // 0 = C, 1 = C#, etc.
+                root = rng.Next(12);
+
+                //Triad, 7th
+                extend = rng.Next(2);
+
+                //Minor, Major, Dim, Aug, 7
+                quality = rng.Next(extend == 1 ? 4 : 5);
+                Console.WriteLine($"{root} {extend} {quality}");
+
+                generateLocation();
+            }
+
+            /// <summary>
+            /// Private method to generate the locations of the chord
+            /// </summary>
+            private void generateLocation()
+            {
+                points = new Point[3 + extend];
+
+                //Start at root of the chord
+                int index = root;
+                for(int i = 0; i < points.Length; i++)
+                {
+                    //Go up a certain number of half steps depending on the chord
+                    index += qualities[quality, i];
+
+                    int x = 17*(index+1);
+                    int y = 0;
+
+                    //Sets the points in the correct position
+                    if(index % 12 >= 5)
+                    {
+                        x += 16;
+                        y = (index+1) % 2 == 0 ? cRoot.Y : 95;
+                    }
+                    else
+                    {
+                        y = (index + 1) % 2 == 1 ? cRoot.Y : 95;
+                    }
+
+                    //If note reaches past an octave, move accordingly
+                    x += index >= 12 ? 32 : 0;
+
+                    points[i] = new Point(x, y);
+                }
+            }
+
+            /// <summary>
+            /// Draw the locations of the notes on the pictureBox
+            /// </summary>
+            /// <param name="g"></param>
+            public void drawLocation(Graphics g)
+            {
+                foreach (Point point in points)
+                {
+                    g.FillEllipse(Brushes.Red, new Rectangle(point.X - radius, point.Y - radius, radius * 2, radius * 2));
+                }
+            }
+
+            /// <summary>
+            /// Retrieves the name of the chord with the given properties
+            /// </summary>
+            /// <returns>Name of the Chord</returns>
+            public string getName()
+            {
+                string[] root = new string[12] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+                string[] quality = new string[6] { "m", "maj", "dim", "aug", "7", ""};
+                string[] extend = new string[2] { "", "7" };
+                return $"{root[this.root]}" +
+                    $"{quality[this.extend == 0 && this.quality == 1 ? 5 : this.quality]}" +
+                    $"{extend[this.extend]}";
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Generate a random chord when opened
+            currChord.generateChord();
+            chordLabel.Text = currChord.getName();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //Draw graphics when asked to reveal
+            if (!reveal)
+                return;
+            g = e.Graphics;
+            currChord.drawLocation(g);
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            //Generates a new chord and refreshes graphics
+            reveal = false;
+            revealButton.Enabled = true;
+            pictureBox1.Invalidate();
+            currChord.generateChord();
+            chordLabel.Text = currChord.getName();
+        }
+
+        private void revealButton_Click(object sender, EventArgs e)
+        {
+            //Reveals the location of the notes
+            reveal = true;
+            revealButton.Enabled = false;
+            pictureBox1.Invalidate();
+
+        }
+    }
+}
